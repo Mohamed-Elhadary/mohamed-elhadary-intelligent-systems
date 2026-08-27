958964173c21e20f83e8f62d0dd2edca1dd290ab
Mohamed Elhadary <128303148+Mohamed-Elhadary@users.noreply.github.com>
2026-08-27 18:09:31 +0300

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mohamed-elhadary-intelligent-systems/",
   server: {
     host: "::",
     port: 8080,
